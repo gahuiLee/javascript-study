e267369d5124efae6550067e0e8a9bc1b9d3158b
LeeGaHeee <gahui0415@gmail.com>
2023-05-05 22:19:35 +0900

    Modify Project

---
+++ js/clock.js
@@ -0,0 +1,13 @@
+const clock = document.querySelector("#clock");
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours());
+  const minutes = String(date.getMinutes());
+  const seconds = String(date.getSeconds());
+
+  clock.innerText = `${hours.padStart(2, "0")} : ${minutes.padStart(2, "0")} : ${seconds.padStart(2, "0")}`
+}
+
+getClock()
+setInterval(getClock, 1000)
